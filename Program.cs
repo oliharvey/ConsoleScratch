4b5ec3be75d6188c4148c940a512c3950651a3f2
OliHarvey <oli@tigertechsoftware.com>
2020-02-02 22:29:35 +0000

    first commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleScratch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World test!");
         }
     }
 }
